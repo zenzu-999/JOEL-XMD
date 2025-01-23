527b6537e97b31d004cdabc5b86d3d22ae5e9054
joel james <162905644+joeljamestech2@users.noreply.github.com>
2025-01-23 22:33:48 +0300

    ᴊᴏᴇʟ ᴍᴅ

--- joel/add.js
+++ joel/add.js
@@ -66,7 +66,7 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         if (!isGroup) return reply('This command can only be used in a group.')
-        if (!isBotAdmins) return reply('Bugatti must be an admin to use this command.')
+        if (!isBotAdmins) return reply('joel md must be an admin to use this command.')
         if (!isAdmins) return reply('You must be an admin to use this command.')
 
         const welcome = q
