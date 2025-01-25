5fb12766181b64f41be5a97f5c198d1e7fe02743
joel james <162905644+joeljamestech2@users.noreply.github.com>
2025-01-25 10:38:22 +0300

    ᴊᴏᴇʟ ᴍᴅ

---
+++ joel/joelxxchannel.js
@@ -0,0 +1,53 @@
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "channel",
+    react: "🙂",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `*┋* ${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `❏━━━━━━━━━━━━━━━━━━━❐
+❒⁠ᴊᴏᴇʟ ᴍᴅ sᴜᴘᴘᴏʀᴛ❑
+https://joeljamestech.netlify.app
+
+❒ᴡᴀ ᴄʜᴀɴɴᴇʟ❑
+*https://whatsapp.com/channel/0029Vak2PevK0IBh2pKJPp2K*
+
+❐ʏᴏᴜ-ᴛᴜʙᴇ❏
+*https://youtube.com/@joeljamestech255*
+
+❐ᴛɪᴋ ᴛᴏᴋ❏
+*https://tiktom.com/@joeljamestech*
+
+❏ɢɪᴛ ʜᴜʙ❐
+*github.com/joeljamestech2*
+
+⁠⁠⁠❏━━━━━━━━━━━━━━━━━━━❐
+*ᴋᴇᴇᴘ ᴜsɪɴɢ ᴊᴏᴇʟ ᴍᴅ ʙᴏᴛ*`
+
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`ғᴏʟʟᴏᴡ ᴍᴇ ᴘʟᴇᴀsᴇ 😞`)
+}
+})
