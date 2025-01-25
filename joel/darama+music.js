d5e9178bbefa27324fcc658164e7711627ccdee2
joel james <162905644+joeljamestech2@users.noreply.github.com>
2025-01-25 10:05:21 +0300

    ᴊᴏᴇʟ ᴍᴅ

--- joel/darama+music.js
+++ joel/darama+music.js
@@ -2,7 +2,7 @@ const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
 cmd({
-    pattern: "play",
+    pattern: "play2",
     desc: "To download songs.",
     react: "🎵",
     category: "download",
